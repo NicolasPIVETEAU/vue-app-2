8fe848e22c153c5f47d8aeb5a2f8d6683ebf344e
Nicolas Piveteau <nicolas.piveteau@epsi.fr>
2021-03-15 08:39:23 +0100

    "Do it !"

---
+++ docs/precache-manifest.58874f665291aed00764d5fb3122d416.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b168c107661ed461a22d",
+    "url": "/vue-app-2/css/app.43ab4fde.css"
+  },
+  {
+    "revision": "c20491d496d52199f0d5",
+    "url": "/vue-app-2/css/chunk-vendors.370ca2bc.css"
+  },
+  {
+    "revision": "0466f5a765822d8f59a614b9605f05b0",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "b168c107661ed461a22d",
+    "url": "/vue-app-2/js/app.67b0ab5f.js"
+  },
+  {
+    "revision": "c20491d496d52199f0d5",
+    "url": "/vue-app-2/js/chunk-vendors.2a1101aa.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
